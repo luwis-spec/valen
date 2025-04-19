4f1679615bc9885298ab150727007dc5ca0311ea
dev_louise <louisreginaldo.bsit@gmail.com>
2025-04-19 10:10:36 +0800

    changes

--- js.js
+++ js.js
@@ -1,25 +1,26 @@
-
-onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
+window.onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
 
   const loginText = document.querySelector(".title-text .login");
   const loginForm = document.querySelector("form.login");
   const loginBtn = document.querySelector("label.login");
   const signupBtn = document.querySelector("label.signup");
   const signupLink = document.querySelector("form .signup-link a");
-  signupBtn.onclick = (()=>{
-    loginForm.style.marginLeft = "-50%";
-    loginText.style.marginLeft = "-50%";
-  });
-  loginBtn.onclick = (()=>{
-    loginForm.style.marginLeft = "0%";
-    loginText.style.marginLeft = "0%";
-  });
-  // signupLink.onclick = (()=>{
-  //   signupBtn.click();
-  //   return false;
-  // });
+
+  if (signupBtn && loginBtn) {
+    signupBtn.onclick = () => {
+      loginForm.style.marginLeft = "-50%";
+      loginText.style.marginLeft = "-50%";
+    };
+
+    loginBtn.onclick = () => {
+      loginForm.style.marginLeft = "0%";
+      loginText.style.marginLeft = "0%";
+    };
+  } else {
+    // console.warn("loginBtn or signupBtn not found in the DOM.");
+  }
+};
